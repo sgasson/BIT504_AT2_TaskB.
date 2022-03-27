0b14e16c6f837b15dfe2847e61853b39dfbe7e6d
sgasson <63143495+sgasson@users.noreply.github.com>
2022-03-27 23:09:49 +1300

    painting

--- src/pong/pongPanel.java
+++ src/pong/pongPanel.java
@@ -5,40 +5,59 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.Timer;
 import javax.swing.JPanel;
-
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class pongPanel extends JPanel implements ActionListener, KeyListener {
-	
+
 	private final static Color BACKGROUND_COLOUR = Color.BLACK;
-	
+	private final static int TIMER_DELAY = 5;
+
 	public pongPanel() {
 		setBackground(BACKGROUND_COLOUR);
-		
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.start();
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent event) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
-		
+		update();
+
+	}
+
+	private void update() {
+	}
+
+	{
+
 	}
 
 }
