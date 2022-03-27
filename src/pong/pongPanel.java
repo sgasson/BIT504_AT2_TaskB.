924d9f9c539574153c8ae8a4667211e5794df25a
sgasson <63143495+sgasson@users.noreply.github.com>
2022-03-27 23:31:17 +1300

    night night

--- src/pong/pongPanel.java
+++ src/pong/pongPanel.java
@@ -1,5 +1,6 @@
 package pong;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,6 +10,7 @@ import javax.swing.Timer;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
 
 public class pongPanel extends JPanel implements ActionListener, KeyListener {
 
@@ -43,15 +45,23 @@ public class pongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.WHITE);
-		g.fillRect(20, 20, 100, 100);
+		paintDottedLine(g);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		update();
-
+		repaint();
 	}
+	
+	private void paintDottedLine(Graphics g) {
+		      Graphics2D g2d = (Graphics2D) g.create();
+		         Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+		         g2d.setStroke(dashed);
+		         g2d.setPaint(Color.WHITE);
+		         g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+		         g2d.dispose();
+		}
 
 	private void update() {
 	}
---
+++ src/pong/sprite.java
@@ -0,0 +1,11 @@
+package pong;
+
+
+
+public class sprite {
+
+	private final static int  xPosition, yPosition;
+	private final static int xVelocity, yVelocity;
+	private final static int width, height;
+	
+}
